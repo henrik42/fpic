b2f44a36edfc462c97c6fd352ef5eab8689031e2
heineh <henrik.heine@msg.group>
2024-03-25 22:06:16 +0100

    Doku und kleiner Umbau

--- minecraft-4.clj
+++ minecraft-4.clj
@@ -4,9 +4,7 @@
 ;; gebaut wird. Wir werden sehen, dass das gar nicht so leicht ist.
 
 (ns minecraft-4
-  (:require [lambdaisland.witchcraft :as wc]
-            [lambdaisland.witchcraft.events :as events]
-            [clojure.java.data :as j]))
+  (:require [lambdaisland.witchcraft :as wc]))
 
 ;; Baut an der Position `at` eine Wand aus Gold. Die X-Breite der Mauer ist 2,
 ;; die Höhe ist ebenfalls 2. Falls die Funktion mit zwei Argeumenten aufgerufen
@@ -15,8 +13,8 @@
 ;;
 ;; Diese Funktion ruft die wc/set-block Funktion *IN* der for-Schleife auf.
 (defn build-wall-1 [at & [xs ys]]
-  (for [x (range (or xs 2))
-        y (range (or ys 2))]
+  (for [x (range (or xs 2))         ;; Schleife über x und y, ergibt die (x,y)-Positionen. Default-Logik: falls xs nicht nil/false ist, 
+        y (range (or ys 2))]        ;; liefert die or-Form 2. Sonst den Wert von xs. Ebenso für ys
     (wc/set-block
      (wc/add at [x y 0])
      :gold-block)))
@@ -26,27 +24,30 @@
 (build-wall-1 (wc/player) 1 4)
 
 ;; Diese Funktion liefert eine Liste mit Maps der Form {:x x :y y}, wobei x die
-;; Wert 0 bis xs-1 (Default 1) und y die Werte 0 bis ys-1 (Default 1) annimmt.
-(defn wall-positions [& {:keys [xs ys]}]
-  (for [x (range (or xs 2))
-        y (range (or ys 2))]
+;; Wert 0 bis xs-1 und y die Werte 0 bis ys-1 annimmt.  
+;; Cool: diese Funktion können wir testen, ohne irgendwelche Blöcke zu setzen.
+(defn wall-positions [xs ys]
+  (for [x (range xs)
+        y (range ys)]
     {:x x :y y}))
 
-;; Baut an der Position `at` eine Wand aus Gold. Die X-Breite der Mauer ist 2,
-;; die Höhe ist ebenfalls 2. Die Funktion kann optional mit `:xs <xs>` und/oder
-;; `:ys <ys>` aufgerufen werden, um die X-Breite und die Höhe anzugeben.
+;; Baut an der Position `at` eine Wand aus Gold. Die X-Breite der Mauer ist
+;; default-mäßig 2, die Höhe ist ebenfalls default-mäßig 2. Die Funktion kann
+;; optional mit `:xs <xs>` und/oder `:ys <ys>` aufgerufen werden, um die
+;; X-Breite und die Höhe explizit zu steuern.
 ;;
 ;; Diese Funktion ruft die Funktion `wall-positions` auf, um die Positionen der
 ;; Wand zu berechnen und ruft dann die Funktion `wc/set-blocks` *EINMALIG* auf,
 ;; um alle Blöcke zu setzen.
-(defn build-wall-2 [at & {:keys [xs ys]}]
-  (let [positionen (->> (wall-positions :xs xs :ys ys)
-                        (map #(conj % [:z 0])))]
+(defn build-wall-2 [at & {:keys [xs ys]         ;; &-Rest -- dadurch optionale Angabe
+                          :or {xs 2 ys 2}}]     ;; durch :or Angabe der Default-Werte
+  (let [positionen (->>                         ;; Thread-Last-Operator: Ergebnisse einer Form in die folgende Form "durchschleusen"
+                    (wall-positions xs ys)      ;; Positionen berechnen
+                    (map #(conj % [:z 0])))]    ;; den Positionen den Eintrag [:z 0] zufügen
     (wc/set-blocks
      positionen
      {:anchor at
       :material :gold-block})))
-
 
 (build-wall-2 (wc/player))
 (build-wall-2 (wc/player) :xs 3)
@@ -55,7 +56,10 @@
 
 ;; Unser erster Versuch, die Blöcke mit einer Verzögerung von einer Sekunde zu
 ;; setzen. Das Funktioniert aber so nicht, weil das wc/set-block erst seine
-;; "Wirkung" entfaltet, wenn die Funktion "fertig ist".
+;; "Wirkung" entfaltet, wenn die Funktion "fertig ist". Wir schlafen also
+;; während die Funktion ausgeführt wird, aber nachdem die Funktion fertig ist,
+;; erscheinen alle Blöcke gleichzeitig. Minecraft zeigt die Blöcke erst an,
+;; *nachdem* die Funktion beendet ist.
 (defn build-wall-3 [at & [xs ys]]
   (for [x (range (or xs 2))        ;; Schleife über x und y
         y (range (or ys 2))]
@@ -82,19 +86,18 @@
 ;; Tasks ausgeführt werden: nur das Einstellen dieser Tasks wird in der Funktion
 ;; erledigt. Die Tasks werden anschließend vom Scheduler mit der eingestellten
 ;; Verzögerung ausgeführt.
-(defn build-wall-4 [at & [xs ys]]
-  (let [at (wc/loc at)]               ;; Da sich `at` (die Spielerposition) über die Zeit ändert, machen wir eine Map raus, die sich nicht ändert!
-    (->>                              ;; Threading-Last-Operator: Ergebnisse werden nacheinander "durchgereicht"
-     (for [x (range (or xs 2))        ;; (x,y)-Positionen berechnen
-           y (range (or ys 2))]
-       [x y])
-     (map-indexed                     ;; map mit Index
-      (fn [i [x y]]                   ;; wird von map-indexed mit (<index> <position>) aufgerufen (<position> wird destructured).
-        (wc/run-task-later            ;; Task einstellen
-         #(wc/set-block               ;; der Task (eine anonyme Funktion, die vom Scheduler ohne Argument aufgerufen wird)
-           (wc/add at [x y 0])        ;; Block setzen
+(defn build-wall-4 [at & {:keys [xs ys]       ;; &-Rest -- dadurch optionale Angabe
+                          :or {xs 2 ys 2}}]   ;; Default-Werte durch :or
+  (let [at (wc/loc at)]                       ;; Da sich `at` (die Spielerposition) über die Zeit ändert, machen wir eine Map raus, die sich nicht ändert!
+    (->>                                      ;; Threading-Last-Operator: Ergebnisse werden nacheinander "durchgereicht"
+     (wall-positions xs ys)                   ;; Positionen berechnen
+     (map-indexed                             ;; map mit Index
+      (fn [i {:keys [x y]}]                   ;; wird von map-indexed mit (<index> <position>) aufgerufen (<position> wird destructured).
+        (wc/run-task-later                    ;; Task einstellen
+         #(wc/set-block                       ;; der Task (eine anonyme Funktion, die vom Scheduler ohne Argument aufgerufen wird)
+           (wc/add at [x y 0])                ;; Block setzen
            :gold-block)
-         (* 5 i))))                   ;; Zeitversatz = 5 * <index>
-     (dorun))))                       ;; map-indexed liefert eine "lazy list". Diese "forcen" wir mit `dorun`
+         (* 5 i))))                           ;; Zeitversatz = 5 * <index> Ticks
+     (dorun))))                               ;; map-indexed liefert eine "lazy list". Diese "forcen" wir mit `dorun`
 
-(build-wall-4 (wc/player) 10 10)
+(build-wall-4 (wc/player) :xs 10 :ys 5)
